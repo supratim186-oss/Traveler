847b7e08d925341b76440efb71b6662b75e4db94
supratim186-oss <supratim186@gmail.com>
2025-12-29 15:41:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+(function(){
+    if(window.innerWidth<=768){
+        alert("This website is designed for desktop / PC users. Please open it on a laptop or PC for the best experience.");
+    }
+})();
+
